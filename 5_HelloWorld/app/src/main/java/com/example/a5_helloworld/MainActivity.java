86646969239321c5b9446b0241af43c84665717a
Kamran Qadir <qadir0108@hotmail.com>
2022-11-13 11:28:31 +0500

    added hello world example project

---
+++ 5_HelloWorld/app/src/main/java/com/example/a5_helloworld/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.a5_helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
