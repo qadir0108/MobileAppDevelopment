f80b8b073f1d74a82b49564700bd0096bc69d02f
Kamran Qadir <qadir0108@hotmail.com>
2023-09-22 00:51:17 +0500

    updated broadcast receiver

--- 9_BroadcastRecievers/app/src/main/java/com/example/a9_broadcastrecievers/MyReceiver.java
+++ 9_BroadcastRecievers/app/src/main/java/com/example/a9_broadcastrecievers/MyReceiver.java
@@ -5,19 +5,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.widget.Toast;
 
 // https://www.geeksforgeeks.org/broadcast-receiver-in-android-with-example/
 public class MyReceiver extends BroadcastReceiver {
 
+    public String TAG = "qadir";
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "Intent Detected.", Toast.LENGTH_LONG).show();
 
         if (isAirplaneModeOn(context.getApplicationContext())) {
             Toast.makeText(context, "AirPlane mode is on", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "AirPlane mode is on");
         } else {
             Toast.makeText(context, "AirPlane mode is off", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "AirPlane mode is off");
         }
     }
 
